d59983431382ff3ec46237a31e517053d8275bc0
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-12 11:31:16 +0200

    removed production

--- glance_minecraft_power/main.py
+++ glance_minecraft_power/main.py
@@ -50,7 +50,6 @@ def main():
     host = os.environ.get("FLASK_HOST", "127.0.0.1")
     port = int(os.environ.get("FLASK_PORT", 5000))
 
-    app.env = "production"
     app.run(host=host, port=port)
 
 
