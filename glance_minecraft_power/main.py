5033763a16c4150953147fe108583d0ddcf99b0b
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-12 10:31:31 +0200

    production

--- glance_minecraft_power/main.py
+++ glance_minecraft_power/main.py
@@ -49,6 +49,8 @@ def stop_server():
 def main():
     host = os.environ.get("FLASK_HOST", "127.0.0.1")
     port = int(os.environ.get("FLASK_PORT", 5000))
+
+    app.env = "production"
     app.run(host=host, port=port)
 
 
